28411c1480d82eef8b18083cb40ee7fded8202e9
Iamneo <techadmin@iamneo.ai>
2023-08-02 10:52:15 +0000

    Backup Commit

--- application/src/main/java/com/examly/springapp/HouseController.java
+++ application/src/main/java/com/examly/springapp/HouseController.java
@@ -1,13 +1,19 @@
-package com.example.housemanagement.controller;
-
-import com.example.housemanagement.exception.HouseNotFoundException;
-import com.example.housemanagement.model.House;
-import com.example.housemanagement.repository.HouseRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+package com.examly.springapp.controller;
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.examly.springapp.exception.ResourceNotFoundException;
+import com.examly.springapp.model.House;
+import com.examly.springapp.repository.HouseRepository;
 
 @RestController
 @RequestMapping("/house")
