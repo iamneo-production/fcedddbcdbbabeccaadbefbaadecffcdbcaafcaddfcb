349032ee30be3d91c22408725a7f230b1c537c76
Iamneo <techadmin@iamneo.ai>
2023-08-02 06:32:36 +0000

    Backup Commit

---
+++ springapp/src/main/java/com/examly/springapp/House.java
@@ -0,0 +1,73 @@
+package com.examly;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class House {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String houseId;
+    private String houseNo;
+    private String status;
+    private String type;
+
+    // Constructors
+
+    public House() {
+        // Default constructor for JPA
+    }
+
+    public House(String houseId, String houseNo, String status, String type) {
+        this.houseId = houseId;
+        this.houseNo = houseNo;
+        this.status = status;
+        this.type = type;
+    }
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getHouseId() {
+        return houseId;
+    }
+
+    public void setHouseId(String houseId) {
+        this.houseId = houseId;
+    }
+
+    public String getHouseNo() {
+        return houseNo;
+    }
+
+    public void setHouseNo(String houseNo) {
+        this.houseNo = houseNo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
